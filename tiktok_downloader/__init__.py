b711d3c17629e58101f5b3db134fb4db4f1b21de
Toihoccode1405 <son26dk9cntt@gmail.com>
2025-04-12 15:49:22 +0700

    Cập nhập tính năng Autoupdate v2

--- tiktok_downloader/__init__.py
+++ tiktok_downloader/__init__.py
@@ -1,2 +1,4 @@
-# Phiên bản của phần mềm
-__version__ = "1.0.0"
+from .downloader import TikTokDownloader
+from .updater import Updater
+
+__version__ = "1.2.2"  # Đảm bảo phiên bản này khớp với setup.py
