27323dfc2f77bd906c951614a7084ac0dbc8844a
Toihoccode1405 <son26dk9cntt@gmail.com>
2025-04-13 16:32:34 +0700

    Update version 1.3.0

--- tiktok_downloader/__init__.py
+++ tiktok_downloader/__init__.py
@@ -1,4 +1,4 @@
 from .downloader import TikTokDownloader
 from .updater import Updater
 
-__version__ = "1.2.2"  # Đảm bảo phiên bản này khớp với setup.py
+__version__ = "1.3.0"  # Đảm bảo phiên bản này khớp với setup.py
